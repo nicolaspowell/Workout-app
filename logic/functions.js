e056b29b4703de081fb8736f1b4fc14da1e53081
nicolaspowell <nicolasapowell@email.com>
2026-09-01 19:32:12 -0500

    some js functions alongside some test data

---
+++ logic/functions.js
@@ -0,0 +1,9 @@
+function calculateVolume(sets, reps, weight) {
+    return sets * reps * weight;
+}
+function addExercise(exercise) {
+    // Implementation to add an exercise
+}
+function removeExercise(exercise) {
+    // Implementation to remove an exercise
+}

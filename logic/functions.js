a87bfbbe669d80c735bdfddec4e563fec1d2acce
nicolaspowell <nicolasapowell@email.com>
2026-09-01 19:46:52 -0500

    some more filtration i need to add and exported the js data to antoher file

--- logic/functions.js
+++ logic/functions.js
@@ -1,3 +1,4 @@
+import { exercises } from './mockdata.js';
 function calculateVolume(sets, reps, weight) {
     return sets * reps * weight;
 }
@@ -6,4 +7,13 @@ function addExercise(exercise) {
 }
 function removeExercise(exercise) {
     // Implementation to remove an exercise
+}
+function filterExercises(workouts){
+    //learn to filer
+}
+function findExercise(name) {
+    //learn to find
+}
+function createArray() {
+    // Implementation to create an array
 }
--- logic/mockdata.js
+++ logic/mockdata.js
@@ -1,7 +1,8 @@
-const exercises = [
+export const exercises = [
     { name: 'Bench Press', muscle: 'Chest'},
     { name: 'Squat', muscle: 'Legs' },
     { name: 'Deadlift', muscle: 'Legs'},
     { name: 'Overhead Press', muscle: 'Shoulders' },
     { name: 'Pull-up', muscle: 'Back' }
 ];
+export { exercises };
